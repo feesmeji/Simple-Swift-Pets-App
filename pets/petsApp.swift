be394a40ecaf477afc65d34f1b82a86a65c23629
Fernando <fernando.est.mejia@gmail.com>
2026-04-05 22:28:29 -0700

    Initial Commit

---
+++ pets/petsApp.swift
@@ -0,0 +1,17 @@
+//
+//  petsApp.swift
+//  pets
+//
+//  Created by Fernando Esteban Mejia on 4/5/26.
+//
+
+import SwiftUI
+
+@main
+struct petsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
